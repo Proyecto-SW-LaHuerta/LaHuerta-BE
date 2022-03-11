29011275749eabc2f313a2bb41a970493d2ad9e0
Tania Pedraza <taniapedraza97@gmail.com>
2022-03-11 18:02:30 -0500

    Bill model, view and serializer updated

--- authApp/views/billView.py
+++ authApp/views/billView.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from authApp.models.bill import Bill
 from authApp.serializers.billSerializer import BillSerializer
 
-# Get all users and post an bill
+# Get all bills and post an bill
 class BillListCreateView(generics.ListCreateAPIView):
     queryset = Bill.objects.all()
     serializer_class = BillSerializer
